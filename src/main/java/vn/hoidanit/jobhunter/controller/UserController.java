5a6b270f5189ec9459e994aa67d96516dc6025af
ducna1 <ducna1@hblab.vn>
2025-02-18 14:50:25 +0700

    @pathvarible delete user 27

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
@@ -18,10 +20,15 @@ public class UserController {
   }
 
   // @GetMapping("/user/create")
-  @PostMapping("/user/create")
+  @PostMapping("/user")
   public User createNewUser(@RequestBody User postManUser) {
-
     User ericUser = this.userService.handleCreateUser(postManUser);
     return ericUser;
+  }
+
+  @DeleteMapping("/user/{id}")
+  public String deleteUser(@PathVariable("id") Long id) {
+    this.userService.handleDeleteUser(id);
+    return "Delete user";
   }
 }
