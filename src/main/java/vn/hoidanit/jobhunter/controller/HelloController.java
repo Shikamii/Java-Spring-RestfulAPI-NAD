4ce6800c0938c35fdfe2ae30af5f9061c2b74515
ducna1 <ducna1@hblab.vn>
2025-02-20 16:49:07 +0700

    pham vi cua excepton 40

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -1,13 +1,25 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 @RestController
 public class HelloController {
 
+    @ExceptionHandler(value = IdInvalidException.class)
+    public ResponseEntity<String> handleBlogAlreadyExistsException(IdInvalidException idException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(idException.getMessage());
+    }
+
     @GetMapping("/")
-    public String getHelloWorld() {
+    public String getHelloWorld() throws IdInvalidException {
+        if (true)
+            throw new IdInvalidException("check");
         return "Hello World NAD with HBlab";
     }
 }
