0cb89e16f89954f028107766fc9e614a670369ca
ducna1 <ducna1@hblab.vn>
2025-02-20 16:12:21 +0700

    throw exception 39

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -42,9 +45,18 @@ public class UserController {
     return ResponseEntity.status(HttpStatus.CREATED).body(ericUser);
   }
 
+  @ExceptionHandler(value = IdInvalidException.class)
+  public ResponseEntity<String> handleBlogAlreadyExistsException(IdInvalidException idException) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(idException.getMessage());
+  }
+
   // delete a user
   @DeleteMapping("/users/{id}")
-  public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+  public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) throws IdInvalidException {
+
+    if (id >= 1500) {
+      throw new IdInvalidException("id ko duoc lon hon 1500");
+    }
     this.userService.handleDeleteUser(id);
     // return "Delete user";
     return ResponseEntity.status(HttpStatus.OK).body("Delete user");
